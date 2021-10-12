1dae284f0f8f22dc9e1f3bd4ab315a99fd353018
vaanres <vaanres@gmail.com>
2021-10-12 13:07:04 +0700

    udpate

--- pages/index.js
+++ pages/index.js
@@ -25,9 +25,9 @@ export default function Home({ articles }) {
         <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
           <blockquote>
             <p className="text-lg font-semibold">
-              “Tailwind CSS is the only framework that I've seen scale on large
+              Tailwind CSS is the only framework that I have seen scale on large
               teams. It’s easy to customize, adapts to any design, and the build
-              size is tiny.”
+              size is tiny.
             </p>
           </blockquote>
           <figcaption className="font-medium">
